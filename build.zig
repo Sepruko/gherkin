e7417723725ed9e0f5078a16c6901f4f775630b7
Sepruko <natsepruko@gmail.com>
2023-08-24 17:49:55 +1000

    fix(build.zig): fix build.zig not exporting module

--- build.zig
+++ build.zig
@@ -1,25 +1,34 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *std.Build) !void {
+    // const target = b.standardTargetOptions(.{});
+    // const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "gherkin",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(lib);
-
-    const lib_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    _ = b.addModule("gherkin", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
 
-    const lib_tests_artifact = b.addRunArtifact(lib_tests);
+    // const static_gherkin = b.addStaticLibrary(.{
+    //     .name = "gherkin",
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const lib_tests_step = b.step("test", "Run gherkin library tests");
-    lib_tests_step.dependOn(&lib_tests_artifact.step);
+    // const step_static_gherkin = b.step("lib-static", "Build the static gherkin library");
+    // step_static_gherkin.dependOn(&static_gherkin.step);
+
+    // const install_static_gherkin = b.addInstallArtifact(static_gherkin, .{});
+    // step_static_gherkin.dependOn(&install_static_gherkin.step);
+
+    const test_gherkin = b.addTest(.{
+        .name = "gherkin-test",
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
+
+    const step_test = b.step("test", "Run gherkin library tests");
+    step_test.dependOn(&b.addRunArtifact(test_gherkin).step);
+
+    // const step_install = b.getInstallStep();
+    // step_install.dependOn(step_static_gherkin);
 }
